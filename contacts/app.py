31a4a7fe4cd3db733f955d77c2d676166c6ed812
calebsyring <caleb@thesyrings.us>
2018-02-05 22:26:11 -0500

    catch up commit

--- contacts/app.py
+++ contacts/app.py
@@ -1,9 +1,79 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import contacts.validation as val
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+    phonenumber = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String, nullable=False)
 
-@app.route("/")
-def hello():
-    username = request.args.get("name", "World")
-    return render_template("hello.html", username=username)
+    def __repr__(self):
+        return self.firstname, self.lastname, self.phonenumber
+
+db.drop_all()
+db.create_all()
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/addcontact', methods=['GET', 'POST'])
+def addcontact():
+    if request.method == 'POST':
+        result = val.ContactSchema().load(request.form)
+        if result.errors:
+            return render_template('form.html', error_messages=result.errors, values=request.form)
+        else:
+            form_firstname=request.form['firstname']
+            newcontact = Contact(firstname=form_firstname, lastname=request.form['lastname'], 
+            email=request.form['email'], phonenumber=request.form['phonenumber'])
+            db.session.add(newcontact)
+            db.session.commit()
+            return redirect(url_for('contacts'))
+    elif request.method == 'GET':
+        return render_template('form.html', values = {}, message='Please enter your contact information below.')
+
+@app.route('/contacts', methods=['GET', 'POST'])
+def contacts():
+    if request.method == 'POST':
+        ids = request.form.getlist('contact_id')
+        Contact.query.filter(Contact.id.in_(ids)).delete(synchronize_session=False)
+        db.session.commit()
+        return redirect(url_for('contacts'))
+    elif request.method == 'GET':
+        return render_template('contacts.html', contacts=Contact.query.all())
+
+#create database
+#db.create_all()
+# #create users
+# admin = User(username='admin', email='admin@example.com')
+# guest = User(username='guest', email='guest@example.net')
+# #add users
+# db.session.add(admin)
+# db.session.add(guest)
+# #commit changes
+# db.session.commit()
+# print('ADMIN USER:' + str(User.query.filter_by(username='admin').first()))
+# #create python category
+# py = Category(name='Python')
+# """There are two posts below. The first post is created already within the python category 
+#     using 'category=py'. The second post is created (as a variable). Then it is appended to
+#     the python category on the next line. I guess these are just two different ways of 
+#     doing it, each more useful in its own situation."""
+# #1st post
+# Post(title='Hello Python!', body='Python is pretty cool', category=py)
+# #2nd post
+# p = Post(title='Snakes', body='Ssssssss')
+# py.posts.append(p)
+# #commit changes
+# db.session.add(py)
+# print('POSTS:' + str(py.posts))
+#delete everything after use (so duplicates aren't created)
--- contacts/tests/test_web.py
+++ contacts/tests/test_web.py
@@ -1,5 +1,5 @@
 import flask_webtest
-from contacts.app import app
+from contacts.app import app, db, Contact
 
 
 class TestWeb:
@@ -7,27 +7,115 @@ class TestWeb:
     def setup_class(self):
         self.app = app
         self.app.testing = True
-        self.client = flask_webtest.TestApp(self.app)
+        self.client = flask_webtest.TestApp(self.app, db=db, use_session_scopes=True)
 
-    def test_hello_world(self):
-        resp = self.client.get('/')
-        doc = resp.pyquery
+    def setup(self):
+        Contact.query.delete()
+        db.session.commit()
 
-        assert 'Hello' == doc('title').text()
-        assert 'Hello World!' == doc('body').text()
-
-    def test_hello_caleb(self):
-        resp = self.client.get('/?name=caleb')
-        doc = resp.pyquery
-
-        assert 'Hello caleb!' == doc('body').text()
-
-    def test_form(self):
-        resp = self.client.get('/')
+    def test_form_success(self):
+        resp = self.client.get('/addcontact')
 
         form = resp.form
-        form['name'] = 'caleb'
+        form['firstname'] = 'john'
+        form['lastname'] = 'doe'
+        form['email'] = 'johndoe@something.org'
+        form['phonenumber'] = '000-000-0000'
         resp = form.submit()
+
+        resp = resp.follow()
+        assert resp.request.url == 'http://localhost/contacts'
+        
+        contact = Contact.query.one()
+        assert contact.firstname == 'john'
+        assert contact.lastname == 'doe'
+        assert contact.email == 'johndoe@something.org'
+        assert contact.phonenumber == '000-000-0000'
+
+    def test_form_fields_ivalid(self):
+        resp = self.client.get('/addcontact')
+
+        form = resp.form
+        form['firstname'] = 'joh3*'
+        form['lastname'] = 'do3#'
+        form['email'] = 'johndoesomethingorg'
+        form['phonenumber'] = '000-000&0000'
+        resp = form.submit()
+
+        error_p = resp.pyquery('p.error')
+        assert error_p.eq(0).text() == 'First name invalid. No numbers or special characters allowed.'
+        assert error_p.eq(1).text() == 'Last name invalid. No numbers or special characters allowed.' 
+        assert error_p.eq(2).text() == 'Not a valid email address.' 
+        assert error_p.eq(3).text() == 'You must have a dash between each group of numbers.' 
+        
+        inputs = resp.pyquery('input')
+        assert inputs.eq(0).val() == 'joh3*'
+        assert inputs.eq(1).val() == 'do3#'
+        assert inputs.eq(2).val() == 'johndoesomethingorg'
+        assert inputs.eq(3).val() == '000-000&0000'
+        assert Contact.query.count() == 0       
+
+    def test_contacts(self):
+        #add contact to database
+        newcontact = Contact(firstname = 'john', lastname = 'doe', 
+        email = 'johndoe@something.org', phonenumber = '000-000-0000')
+        db.session.add(newcontact)
+        db.session.commit()
+        
+        resp = self.client.get('/contacts')
+        doc = resp.pyquery
+        
+        trs = doc('tr')
+        assert len(trs) == 2
+        
+        #test header row
+        header_tr = trs.eq(0)
+        header_th = header_tr.find('th')
+        assert header_th.eq(0).text() == ''
+        assert header_th.eq(1).text() == 'First Name'
+        assert header_th.eq(2).text() == 'Last Name'
+        assert header_th.eq(3).text() == 'Email'
+        assert header_th.eq(4).text() == 'Phone #'
+
+        #test contact row
+        contact_tr = trs.eq(1)
+        contact_td = contact_tr.find('td')
+        input = contact_td.eq(0).find('input')
+        assert input.attr('type') == 'checkbox'
+        assert input.attr('name') == 'contact_id'
+        assert input.val() == str(newcontact.id)
+        assert contact_td.eq(1).text() == 'john'
+        assert contact_td.eq(2).text() == 'doe'
+        assert contact_td.eq(3).text() == 'johndoe@something.org'
+        assert contact_td.eq(4).text() == '000-000-0000'
+        
+    def test_delete_contact(self):
+        #add contact to database
+        newcontact = Contact(firstname = 'john', lastname = 'doe', 
+        email = 'johndoe@something.org', phonenumber = '000-000-0000')
+        db.session.add(newcontact)
+        db.session.commit()
+
+        resp = self.client.get('/contacts')
         doc = resp.pyquery
 
-        assert 'Hello caleb!' == doc('body').text()
+        #define number of table rows in contacts table as variable
+        contact_trs = len(Contact.query.all())
+    
+        #delete contact
+        Contact.query.filter_by(id=1).delete()
+        db.session.commit()
+
+        #make sure one less contact in table
+        current_contact_trs = contact_trs - 1
+        assert len(Contact.query.all()) == current_contact_trs
+
+        #refresh page
+        resp = self.client.get('/contacts')
+        #make sure no contacts displayed
+        resp.mustcontain('No contacts to show')
+    
+
+       
+        
+        
